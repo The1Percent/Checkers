3d87e08164e4a66c59c34ea80d2c2a5638692fc0
Chris Penner <cwpenner@asu.edu>
2017-05-01 22:43:43 -0600

    Fixed PlaySound method

--- Checkers/src/checkers/PlaySound.java
+++ Checkers/src/checkers/PlaySound.java
@@ -1,27 +1,13 @@
 package checkers;
 
-import com.apple.eio.FileManager;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
 
-import java.io.File;
 import java.io.IOException;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class PlaySound extends Thread {
 
 	private String filename;
-
-	private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
-
-	enum Position {LEFT, RIGHT, NORMAL}
 
 	public PlaySound(String wavfile) {
 		filename = wavfile;
